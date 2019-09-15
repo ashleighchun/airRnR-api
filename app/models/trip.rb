fc632346a2efb6573856171266c2b4fa27d8666b
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-14 21:30:50 -0700

    add trip validations associations and update method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,15 @@
 class Trip < ApplicationRecord
+  has_many :bookings
+  validates :name, presence: true
+
+  def update_spending_money(booking)
+    if self.budget >= booking.cost
+      self.budget = self.budget - booking.cost
+      self.save
+    else
+      return "Booking exceeds budget, rework those numbers!"
+    end
+  end
+
+
 end
