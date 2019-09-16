c7257018e7742f840397d97a0e5237d39e5b2bc2
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-15 19:40:41 -0700

    update file structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace:v1 do
-      resources :trips do
-        resources :bookings
-      end
-    end
-  end
+  resources :bookings
+  resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
