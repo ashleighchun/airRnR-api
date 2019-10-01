66acbccea382e81fcc3db531a5f6b38c734aba39
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-10-01 02:12:02 -0400

    update schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-trip1 = Trip.create(name: "best wedding ever", trip_type: "wedding", location: "Utah", trip_cost: 2000)
-booking1 = Booking.create(trip_id: 1, booking_type: "flight", start_date: '2020-03-01', end_date: '2020-03-15', booking_cost: 1000, details: "uncles wedding, round trip")
-booking2 = Booking.create(trip_id: 1, booking_type: "hotel", start_date: '2020-03-01', end_date: '2020-03-15', booking_cost: 1000, details: "hotel at resort for wedding night")
+Trip.create([
+  { trip_type: 'day trip' },
+  { trip_type: 'vacation' },
+  { trip_type: 'wedding' },
+  { trip_type: 'funeral' },
+  { trip_type: 'business trip' },
+  { trip_type: 'family visit' },
+  { trip_type: 'sporting event' }
+])
 
-trip2 = Trip.create(name: "aquarium day", trip_type: "day trip", location: "monterey aquarium", trip_cost: 100)
-booking3 = Booking.create(trip_id: 2, booking_type: "car rental", start_date: '2020-04-01', end_date: '2020-04-01', booking_cost: 50, details: "bring Johnny to the aquarium for a field trip")
-booking4 = Booking.create(trip_id: 2, booking_type: "admission", start_date: '2020-04-01', end_date: '2020-04-01', booking_cost: 50, details: "entry to the aquarium")
+Booking.create([
+  { booking_type: 'activity', start_date: '2020-03-01', end_date: '2020-03-1', booking_cost: 800, details: 'rock climbing excursion' },
+  { booking_type: 'flight', start_date: '2020-03-01', end_date: '2020-03-15', booking_cost: 1000, details: 'uncles wedding, round trip' },
+  { booking_type: 'hotel', start_date: '2020-03-01', end_date: '2020-03-15', booking_cost: 1000, details: 'hotel at resort for wedding night' },
+  { booking_type: 'car rental', start_date: '2020-04-01', end_date: '2020-04-01', booking_cost: 50, details: 'bring Johnny to the aquarium for a field trip' },
+  { booking_type: 'admission', start_date: '2020-04-01', end_date: '2020-04-01', booking_cost: 50, details: 'entry to the aquarium' }
+  ])
